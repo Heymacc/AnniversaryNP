c90322e157e701a7a66c2b9d1cab446ad1c6a3bc
Heymacc <bxydoncry@gmail.com>
2024-10-16 01:49:29 +0700

    Add files via upload

---
+++ ans.js
@@ -0,0 +1,4 @@
+document.getElementById("messageButton").addEventListener("click", function() {
+    const messageDiv = document.getElementById("message");
+    messageDiv.classList.toggle("hidden");
+});
